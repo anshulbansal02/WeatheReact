92f4dc009fd7512eb159a0809ae0e24cbbe5150f
Anshul Bansal <55446476+anshulbansal02@users.noreply.github.com>
2020-10-06 20:46:06 +0530

    Update unsplash.js

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
-    headers: { authorization: 'Client-ID 5382fd105af4991e3ec81825b2ea0be8a2919eb740b124b5f575754c61a7cadc'}
+    headers: { authorization: 'Client-ID '}
 });
